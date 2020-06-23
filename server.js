8ff412f873f68404f9c9a316ecbac6c47df0b163
Alex Walker <alexanderrules2015@gmail.com>
2020-06-23 17:05:22 +0100

    Initial Commit

--- server.js
+++ server.js
@@ -10,21 +10,8 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 app.get("*", (req, res) => {
-  res.status(404).send("what???");
-});
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error("Not Found");
-  err.status = 404;
-  next(err);
-});
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.render("error", {
-    message: err.message,
-    error: {},
-  });
+  res.status(404).send("Page Not Found");
 });
 app.listen(3000, () => {
-  console.log("Listening on 3000");
+  console.log("Listening on 3000!");
 });
