2b2a2da0d5a77728210af5c286e4efde0b914dbb
Alex Walker <alexanderrules2015@gmail.com>
2020-07-11 01:17:28 +0100

    revert me

--- server.js
+++ server.js
@@ -2,16 +2,15 @@ var express = require("express");
 var path = require("path");
 var app = express();
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 // Routing
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.sendFile(__dirname + "/views/index.html");
 });
 app.get("*", (req, res) => {
   res.status(404).send("Page Not Found");
 });
-app.listen(3000, () => {
-  console.log("Listening on 3000!");
+app.listen(80, () => {
+  console.log("Listening on 80!");
 });
