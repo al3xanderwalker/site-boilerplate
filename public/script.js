f8b18fb3d530fa346c2084fab3ff061da123aaa2
al3xanderwalker <48996881+al3xanderwalker@users.noreply.github.com>
2020-09-24 19:29:29 +0100

    Socket IO, removed bootstrap

---
+++ public/script.js
@@ -0,0 +1,3 @@
+/* Socket IO
+var socket = io();
+*/
--- server.js
+++ server.js
@@ -1,17 +1,33 @@
-var express = require("express");
-var path = require("path");
+var express = require('express');
+var path = require('path');
 var app = express();
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 // Routing
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
-app.get("*", (req, res) => {
-  res.status(404).send("Page Not Found");
+app.get('*', (req, res) => {
+  res.status(404).send('Page Not Found');
 });
+
 app.listen(3000, () => {
-  console.log("Listening on 3000!");
+  console.log('listening on 3000');
 });
+
+/* Socket IO
+
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+http.listen(3000, () => {
+  console.log('listening on 3000');
+});
+*/
